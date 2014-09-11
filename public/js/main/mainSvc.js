c872477c5737c4c8440a296e5a844d531902da5d
Brendan Quinn <brendanquinn@Brendans-MacBook-Air.local>
2014-09-11 13:57:55 -0400

    delete api

--- public/js/main/mainSvc.js
+++ public/js/main/mainSvc.js
@@ -1,10 +1,10 @@
 angular.module("mainController")
-  .factory("mainSvc", function ($rootScope, $log) {
-      // return $resource('api/collections/people',
-			// {},
-			// {
-			// 	query: { method: 'GET', isArray: true },
-			// 	create: { method: 'POST'}
-			// });
+  .factory("mainSvc", function ($resourse, $rootScope, $log) {
+      return $resource('collections/people',
+			{},
+			{
+				query: { method: 'GET', isArray: true },
+				create: { method: 'POST'}
+			});
 
   });
