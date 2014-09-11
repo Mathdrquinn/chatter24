ffed301080083f05230ece873b027e7deb5d2ef9
Natalie Miller <nataliemiller03@gmail.com>
2014-09-11 12:55:08 -0400

    testing merge conflicts

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular
-  .module('ngDay2App', [
+  .module('chatApp', [
     'ngCookies',
     'ngResource',
     'ngSanitize',
@@ -11,22 +11,6 @@ angular
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html'
-      })
-      .when('/blog', {
-        templateUrl: 'views/blog-list.html',
-        controller: 'PostsCtrl'
-      })
-      .when('/new', {
-        templateUrl: 'views/blog-create.html',
-        controller: 'PostsCtrl'
-      })
-      .when('/blog/:id', {
-        templateUrl: 'views/blog-detail.html',
-        controller: 'PostCtrl'
-      })
-      .when('/blog/:id/edit', {
-        templateUrl: 'views/blog-editDetail.html',
-        controller: 'PostCtrl'
       })
       .otherwise({
         redirectTo: '/'
