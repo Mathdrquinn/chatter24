64c07388bd0f9cdd8090f3819ff7241cf25d2156
Brendan Quinn <brendanquinn@Brendans-MacBook-Air.local>
2014-09-11 13:56:24 -0400

    fix the bug

--- public/js/main/mainCtrl.js
+++ public/js/main/mainCtrl.js
@@ -1,5 +1,5 @@
 angular.module("mainController")
-    .controller("mainCtrl", function ($rootScope, $scope, $location, $routeParams, $location, mainSvc) {
+    .controller("mainCtrl", function ($rootScope, $scope, $location, $routeParams, mainSvc) {
 
       $scope.createPerson = function (person){
         mainSvc.create(person);
--- public/js/main/mainSvc.js
+++ public/js/main/mainSvc.js
@@ -1,10 +1,10 @@
 angular.module("mainController")
-  .factory("mainSvc", function ($resource, $rootScope, $log) {
-      return $resource('api/collections/people',
-			{},
-			{
-				query: { method: 'GET', isArray: true },
-				create: { method: 'POST'}
-			});
+  .factory("mainSvc", function ($rootScope, $log) {
+      // return $resource('api/collections/people',
+			// {},
+			// {
+			// 	query: { method: 'GET', isArray: true },
+			// 	create: { method: 'POST'}
+			// });
 
   });
