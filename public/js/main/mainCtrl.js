050e731736caa2f6fea41b814a3fb103b85515f4
Natalie Miller <nataliemiller03@gmail.com>
2014-09-11 13:29:42 -0400

    added createPerson function

--- public/js/main/mainCtrl.js
+++ public/js/main/mainCtrl.js
@@ -1,9 +1,16 @@
 angular.module("mainController")
     .controller("mainCtrl", function ($rootScope, $scope, $location, $routeParams, $location, mainSvc) {
 
+      $scope.createPerson = function (person){
+        mainSvc.create(person);
+        $rootScope.$broadcast('person:added');
+        $location.path('/lobby');
+
+      };
+      $scope.people = mainSvc.query();
 
 
-      
+
 
 
 
