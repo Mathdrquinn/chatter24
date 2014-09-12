0c0338262876cd086af79a6e923a55ada97ecf11
Brendan Quinn <brendanquinn@Brendans-MacBook-Air.local>
2014-09-12 12:21:53 -0400

    added user variable

--- public/js/main/mainCtrl.js
+++ public/js/main/mainCtrl.js
@@ -1,19 +1,25 @@
 angular.module("mainController")
     .controller("mainCtrl", function ($rootScope, $scope, $location, $routeParams, mainSvc, roomsSvc) {
 
-      // $scope.user = "undefined";
+      $scope.user = function (x) {
+        var length = $scope.people.length
+        var y = x.name
+        for(var i = 0; i < length, i++) {
+          if (scope.people[i].name === y) {
+            return x;
+          }
+          else {
+            $scope.createPerson = function (x){
 
-      $scope.createPerson = function (person){
-        mainSvc.create(person);
-        $rootScope.$broadcast('person:added');
-        $location.path('/lobby');
-        // $scope.user = person.name;
-        // console.log($scope.user);
+              mainSvc.create(person);
+              $rootScope.$broadcast('person:added');
+              $location.path('/lobby');
+              return x;
 
-      };
-      $scope.deletePerson = function (_id){
-        mainSvc.delete(_id);
-      };
+            };
+          }
+        }
+      }
 
       $scope.people = mainSvc.query();
 
@@ -23,7 +29,6 @@ angular.module("mainController")
         //$location.path('/lobby');
 
       };
-
 
       $scope.rooms = roomsSvc.query();
 
